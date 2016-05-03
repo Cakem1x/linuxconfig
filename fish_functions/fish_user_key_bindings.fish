6f0af4017fe9837ac8991d3b605df1164b3eead3
Cakemix <mholoch@gmail.com>
2016-05-03 13:42:41 +0200

    added a silly fish_greeting function implementation, fixed vi keybindings plus user keybindings, added a function for prettyping.

--- fish_functions/fish_user_key_bindings.fish
+++ fish_functions/fish_user_key_bindings.fish
@@ -1,3 +1,6 @@
 function fish_user_key_bindings
+  # this sets all the things needed for vi mode, also erases all other keybindings
+  fish_vi_key_bindings
+  # After that, we can add our own, custom keybindings
 	bind \cs 'prepend_command sudo'
 end
